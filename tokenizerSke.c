e3f15968bf2c5be71655f81e3cd01784009db70f
Elton Villa <euvilla@miners.utep.edu>
2021-09-16 10:49:49 -0600

    Testing git push

--- tokenizerSke.c
+++ tokenizerSke.c
@@ -2,26 +2,46 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
+/*Return true (non-zero) if c is a whitespace character
+	('\t or ' ').
+	Zero terminators are not printable (therefore false) */
 bool delim_character (char c){
 
 }
 
+/*Return true (non-zero) if c is a whitespace character
+	(not tab or space).
+	Zero terminators are not printable (therefore false) */
 bool non_delim_character (char c){
 
 }
 
+/*Returns a pointer to the first character of the next
+	space-separated word*/
 char *word_start (char* str){
 
 }
 
+/*Returns a pointer to the first space character of the zero
+	 terminated string*/
 char *end_word (char* str){
 
 }
 
+//Counts the number of words or tokens
 int count_tokens(char* str){
 
 }
 
+/*Returns a freshly allocated zero-terminated vector of freshly allocated
+space-separated tokens from zero-terminated str.
+For example, tokenize("hello world string") would result in:
+	tokens[0] = "hello"
+	tokens[1] = "world"
+	tokens[2] = "string"
+	tokens[3] = 0
+
+*/
 char *copy_str (char *inStr, short len){
 
 }
